f13c3b790840759c018afe02d24b2ab42ae6655f
Nicolas Hedger <nicolas@hedger.ch>
2023-05-18 13:53:26 +0200

    fix: incorrect comparison

--- src/setup.ts
+++ src/setup.ts
@@ -80,7 +80,7 @@ const download = async (options: SetupOptions): Promise<string> => {
         const assetURL = await findAsset(releaseId, options);
         return await downloadTool(assetURL);
     } catch (error) {
-        if (typeof error === RequestError) {
+        if (error instanceof RequestError) {
             const requestError = error as RequestError;
             if (
                 requestError.status === 403 &&
@@ -120,7 +120,7 @@ const findRelease = async (options: SetupOptions) => {
             })
         ).data.id;
     } catch (error) {
-        if (typeof error === RequestError) {
+        if (error instanceof RequestError) {
             const requestError = error as RequestError;
             if (requestError.status === 404) {
                 throw new Error(
